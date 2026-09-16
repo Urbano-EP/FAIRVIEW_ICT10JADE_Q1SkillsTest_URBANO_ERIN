d3b549cedb7fd9c98c525755ecc3392768478cd0
Urbano-EP <urbano.erp@obmontessori.edu.ph>
2026-09-16 21:27:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 from pyscript import document, display
 
-def create_order(e):
-    document.getElementById("output2").innerHTML = "" #clear previous result
-
-    prod1=document.getElementById("item1") #get item 1 value
-    prod2=document.getElementById("item2") #get item 2 value
-    prod3=document.getElementById("item3") #get item 3 value
-    prod4=document.getElementById("item4") #get item 4 value
-    #Calculate
-    subtotal = (float(prod1.value) * prod1.checked
-                + float(prod2.value) * prod2.checked
-                + float(prod3.value) * prod3.checked
-                + float(prod4.value) * prod4.checked)
-    size = document.querySelector("input[name='size']:checked")
-    price = float(size.value)
-    grandtotal = subtotal + price
-    display(grandtotal, target="output2")
-
 def show_order(e):
     document.getElementById("output4").innerHTML = ""
     # clears previous result
@@ -33,4 +16,7 @@ def show_order(e):
     size = document.querySelector("input[name='size']:checked")
     price = float(size.value)
     grandtotal = subtotal + price + tax
-    display(f'You have to pay a total of {grandtotal}', target="output4")
+    display(f"Subtotal: {subtotal}.", target="output4")
+    display(f"Tax: {tax}.", target="output4", append=True)
+    display(f"Total: {grandtotal}.", target="output4", append=True)
+    display("Thank you for your order!", target="output4", append=True)
