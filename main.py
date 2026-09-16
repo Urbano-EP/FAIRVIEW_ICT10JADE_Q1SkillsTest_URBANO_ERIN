759d0a573471a7135b7c9b0d47d30e923b145709
Urbano-EP <urbano.erp@obmontessori.edu.ph>
2026-09-16 21:08:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,14 @@ def create_order(e):
     prod3=document.getElementById("item3") #get item 3 value
     prod4=document.getElementById("item4") #get item 4 value
     #Calculate
-    subtotal = float(prod1.value) * prod1.checked
-    subtotal = float(prod2.value) * prod2.checked
-    subtotal = float(prod3.value) * prod3.checked
-    subtotal = float(prod4.value) * prod4.checked
+    subtotal = (float(prod1.value) * prod1.checked
+                + float(prod2.value) * prod2.checked
+                + float(prod3.value) * prod3.checked
+                + float(prod4.value) * prod4.checked)
     size = document.querySelector("input[name='size']:checked")
     price = float(size.value)
     grandtotal = subtotal + price
     display(grandtotal, target="output2")
-
-def place_order(e):
-    document.getElementById("output3").innerHTML = "" #clear previous result
-
-    ice_cream = document.getElementById("ice_cream")
-    ice_cream_price = float(ice_cream.value)
-    display(ice_cream_price, target="output3")
 
 def show_order(e):
     document.getElementById("output4").innerHTML = ""
@@ -31,14 +24,13 @@ def show_order(e):
     prod2=document.getElementById("item2") #get item 2 id
     prod3=document.getElementById("item3") #get item 3 id
     prod4=document.getElementById("item4") #get item 4 id
-    subtotal= float(prod1.value) * prod1.checked
-    subtotal += float(prod2.value) * prod2.checked
-    subtotal += float(prod3.value) * prod3.checked
-    subtotal += float(prod4.value) * prod4.checked
+    subtotal = (float(prod1.value) * prod1.checked
+                + float(prod2.value) * prod2.checked
+                + float(prod3.value) * prod3.checked
+                + float(prod4.value) * prod4.checked)
+    taxrate = 0.12
+    tax = subtotal * taxrate
     size = document.querySelector("input[name='size']:checked")
     price = float(size.value)
-    grandtotal = subtotal + price
-    ice_cream = document.getElementById("ice_cream")
-    ice_cream_price = float(ice_cream.value)
-    final_order = grandtotal + ice_cream_price
-    display(f'You have to pay a total of {final_order}', target="output4")
+    grandtotal = subtotal + price + tax
+    display(f'You have to pay a total of {grandtotal}', target="output4")
