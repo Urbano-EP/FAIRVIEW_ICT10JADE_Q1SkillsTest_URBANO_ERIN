f70a66c890f8d093ad630965feed4bd5c041bc9b
Urbano-EP <urbano.erp@obmontessori.edu.ph>
2026-09-16 12:12:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ def create_order(e):
     document.getElementById("output2").innerHTML = "" #clear previous result
 
     prod1=document.getElementById("item1") #get item 1 value
+    prod2=document.getElementById("item2") #get item 2 value
+    prod3=document.getElementById("item3") #get item 3 value
+    prod4=document.getElementById("item4") #get item 4 value
     #Calculate
     subtotal= float(prod1.value) * prod1.checked
+    subtotal += float(prod2.value) * prod2.checked
+    subtotal += float(prod3.value) * prod3.checked
+    subtotal += float(prod4.value) * prod4.checked
     size = document.querySelector("input[name='size']:checked")
     price = float(size.value)
     grandtotal = subtotal + price
@@ -14,9 +20,9 @@ def create_order(e):
 def place_order(e):
     document.getElementById("output3").innerHTML = "" #clear previous result
 
-    coffee = document.getElementById("coffee")
-    coffee_price = float(coffee.value)
-    display(coffee_price, target="output3")
+    ice_cream = document.getElementById("ice_cream")
+    ice_cream_price = float(ice_cream.value)
+    display(ice_cream_price, target="output3")
 
 def show_order(e):
     document.getElementById("output4").innerHTML = ""
@@ -26,7 +32,7 @@ def show_order(e):
     size = document.querySelector("input[name='size']:checked")
     price = float(size.value)
     grandtotal = subtotal + price
-    coffee = document.getElementById("coffee")
-    coffee_price = float(coffee.value)
-    final_order = grandtotal + coffee_price
+    ice_cream = document.getElementById("ice_cream")
+    ice_cream_price = float(ice_cream.value)
+    final_order = grandtotal + ice_cream_price
     display(f'You have to pay a total of {final_order}', target="output4")
